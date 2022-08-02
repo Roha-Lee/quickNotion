c8be72584649962d23a0ce4d7ef5ef2447b17487
Roha-Lee <roha.lee@buzzvil.com>
2022-08-02 23:27:22 +0900

    Notion의 Databases를 가져와서 살펴봅니다.

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import { Client } from "@notionhq/client"
 
 const notion = new Client({ auth: process.env.NOTION_KEY })
-console.log(notion);
+// console.log(notion);
+const response = await notion.databases.retrieve({ database_id: "" });
+console.log(response);
